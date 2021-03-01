b043cc1d436477278fec82970f0e94ba04b70264
Leon Tadina <leon.tadina@gmail.com>
2021-03-01 22:08:11 +0000

    added object skeletons and registration feature (missing fireabse deps)

--- lib/fridge/FridgeScreen.dart
+++ lib/fridge/FridgeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:save_the_scran/camera/TakePictureScreen.dart';
 
-
 class FridgeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,23 +11,21 @@ class FridgeScreen extends StatelessWidget {
         title: Text('My Fridge', style: TextStyle(color: Colors.white)),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-            Navigator.pushNamed(context, '/camera');
+        onPressed: () {
+          Navigator.pushNamed(context, '/camera');
         },
         child: Icon(Icons.add),
         backgroundColor: Color(0xFFFF4081),
       ),
       body: Center(
-        // child: ElevatedButton(
-        //   child: Text('Community Market'),
-        //   onPressed: () {
-        //     // Navigate to the second screen using a named route.
-        //     Navigator.pushNamed(context, '/market');
-        //   },
-        // ),
-      ),
+          // child: ElevatedButton(
+          //   child: Text('Community Market'),
+          //   onPressed: () {
+          //     // Navigate to the second screen using a named route.
+          //     Navigator.pushNamed(context, '/market');
+          //   },
+          // ),
+          ),
     );
   }
 }
-
-
---
+++ lib/models/Fridge.dart
@@ -0,0 +1,8 @@
+class Fridge {
+  List<Item> items;
+
+  Fridge() {
+    this.items = new List<Item>();
+  }
+  List<Item> get getItems => this.items;
+}
