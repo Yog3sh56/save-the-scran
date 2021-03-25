5ad221a89ee6fd0844bc3ade5b49d9d6bf439cce
Thomas Kuhn <59689552+Toqn@users.noreply.github.com>
2021-03-25 14:57:43 +0000

    updated message bubble colors

--- lib/chat/MessageBubbleWidget.dart
+++ lib/chat/MessageBubbleWidget.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   MessageBubble(this.message, this.myId);
- // MessageBubble(this.message, this.isMe, {key}) : super(key: key);
+  // MessageBubble(this.message, this.isMe, {key}) : super(key: key);
 
   final dynamic message;
   final String myId;
-  bool isMe=false;
-  
+  bool isMe = false;
+
   @override
   Widget build(BuildContext context) {
-    if (message['senderId']==myId) isMe=true;
+    if (message['senderId'] == myId) isMe = true;
     return Row(
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: isMe ? Colors.white : Theme.of(context).accentColor,
-            boxShadow:[BoxShadow(color: isMe?Colors.black.withOpacity(0.2):Colors.grey,offset: Offset.fromDirection(1,2))],
+            color: isMe ? Colors.white : Color(0xFFff79af),
+            boxShadow: [
+              BoxShadow(
+                  color: isMe ? Colors.black.withOpacity(0.2) : Colors.grey,
+                  offset: Offset.fromDirection(1, 2))
+            ],
             borderRadius: BorderRadius.only(
-              
               topLeft: Radius.circular(12),
               topRight: Radius.circular(12),
               bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
@@ -31,13 +34,12 @@ class MessageBubble extends StatelessWidget {
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
           child: Text(
             message['text'],
-            
             style: TextStyle(
-              
-                color: isMe
-                    ? Colors.black
-                    : Theme.of(context).accentTextTheme.headline1.color),
-                    
+              color: Colors.black,
+              // color: isMe
+              //     ? Colors.black
+              //     : Theme.of(context).accentTextTheme.headline1.color
+            ),
           ),
         ),
       ],
